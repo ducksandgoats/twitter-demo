ba324fcf9c18f2f9e8af7eb16a855af2b06510c4
ducksandgoats <mrtraderjoseph@gmail.com>
2025-02-16 18:42:58 -0600

    update

--- src/dir/data.js
+++ src/dir/data.js
@@ -1,5 +1,5 @@
 import Dexie from 'relay-dexie'
 
 const dexie = new Dexie({debug: true, version: 1, proto: 'msg:', id: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'test', schema: {posts: 'iden, stamp, edit, user, text, *list, *for, *tags'}, sync: true})
-setInterval(() => {dexie.initUser().then(console.log).catch(console.error)}, 10000)
+setTimeout(() => {dexie.initUser().then(console.log).catch(console.error)}, 10000)
 export {dexie}
