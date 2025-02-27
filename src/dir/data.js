12de6dec054bc9de17775d3a4cd6ce32ca1fbd17
ducksandgoats <mrtraderjoseph@gmail.com>
2025-02-27 01:19:30 -0600

    update

--- src/dir/data.js
+++ src/dir/data.js
@@ -2,7 +2,9 @@ import Dexie from 'relay-dexie'
 
 const obj = {'bt:': 'msg:', 'hyper:': 'topic:', 'ipfs:': 'pubsub:'}
 // const proto = Object.keys(obj).includes(window.location.protocol) ? obj[window.location.protocol] : null
+const proto = 'msg:'
 // const id = window.location.hostname
-const dexie = new Dexie({debug: true, version: 1, proto: 'msg:', id: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'test', schema: {posts: 'iden, stamp, edit, user, text, *list, *for, *tags'}, sync: true})
+const id = 'testing'
+const dexie = new Dexie({debug: true, version: 1, ben: 'str', proto, id, name: 'test', schema: {posts: 'iden, stamp, edit, user, text, *list, *for, *tags'}, sync: true})
 setTimeout(() => {dexie.initUser().then(console.log).catch(console.error)}, 10000)
 export {dexie}
